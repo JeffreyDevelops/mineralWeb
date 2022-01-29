2effa3ebbf0a8f882600664e47945af111fae1f4
Jeffrey <88632169+JeffreyEj@users.noreply.github.com>
2022-01-29 02:02:57 +0100

    added skins to leaderboards per player

--- nodeapp/routes/users.js
+++ nodeapp/routes/users.js
@@ -3,13 +3,19 @@ let router = express.Router();
 let db=require('../database');
 // another routes also appear here
 // this script to fetch data from MySQL databse table
+
+
 router.get('/leaderboard', function(req, res, next) {
-    let sql='SELECT * FROM elo';
+    let sql='SELECT * FROM leaderboard';
     db.query(sql, function (err, data, fields) {
     if (err) throw err;
     res.render('leaderboard', { title: 'leaderboard_user', userData: data});
   });
+  
+
+
 });
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {

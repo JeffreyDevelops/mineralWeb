6d807f96644c8f0f3bc59256b7ee1cbd72afd863
Jeffrey <88632169+JeffreyEj@users.noreply.github.com>
2022-03-02 19:45:00 +0100

    fixed crashing when typing random numbers on letters (version 1)

--- nodeapp/routes/leaderboard.js
+++ nodeapp/routes/leaderboard.js
@@ -12,28 +12,36 @@ const resultsPerPage = 15;
 router.get('/', function(req, res, next) {
   let sql='SELECT * FROM leaderboard WHERE Gametype="Nodebuff" ORDER BY ELO DESC';
   db.query(sql, function (err, data, fields) {
-  if (err) throw err;
+  if (err) {
+    global.location = "http://localhost:3000/leaderboard/?page=1";
+  }
+
   const numOfResults = data.length;
   const numberofPages = Math.ceil(numOfResults / resultsPerPage);
   let page = req.query.page ? Number(req.query.page) : 1;
   if(page > numberofPages) {
     res.redirect("http://localhost:3000/leaderboard/?page="+encodeURIComponent(numberofPages));
   } else if (page < 1) {
-    res.redirect("http://localhost:3000/leaderboard/?page="+encodeURIComponent("1"));
+    global.location = "http://localhost:3000/leaderboard/?page=1";
   }
+  
+  
   //Determine the SQL LIMIT starting number
-  const startingLimit = (page - 1) * resultsPerPage;
+  const startingLimit = (page - 1) * resultsPerPage; 
   // Get the releavant number of POSTS for this starting page
   sql = `SELECT * FROM leaderboard WHERE Gametype="Nodebuff" ORDER BY ELO DESC LIMIT  ${startingLimit}, ${resultsPerPage}`;
   db.query(sql, (err, data) =>{
-    if (err) throw err;
+    if (err) {
+      global.location = "http://localhost:3000/leaderboard/?page=1";
+    } 
+      
     let iterator = (page - 3) < 1 ? 1 : page;
     let endingLink = (iterator + 5) <= numberofPages ? (iterator + 5) : page + 
     (numberofPages - page);
     if(endingLink < (page + 4)) {
       iterator -= (page + 4) - numberofPages;
     }
-    res.render('leaderboard', {userData: data, page, iterator, endingLink, numberofPages});
+    res.render('leaderboard', {userData: data, page, iterator, endingLink, numberofPages});   
   });
    
 });
--- nodeapp/routes/player.js
+++ nodeapp/routes/player.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+let db=require('../database');
 
 /* GET player page. */
 router.get('/', function(req, res, next) {
-    res.render('player', { title: 'Player' });
+    let sql='SELECT PLAYER FROM leaderboard';
+    db.query(sql, function (err, data, fields) {
+    if (err) throw err;
+  let resultsPerPage = data.PLAYER;
+  const numOfResults = data.PLAYER;
+
+    if (resultsPerPage === numOfResults)
+    res.redirect("http://localhost:3000/player/?player="+encodeURIComponent(numOfResults));
+  
+    res.render('player', {userData: data});
+});
 });
 
 module.exports = router;

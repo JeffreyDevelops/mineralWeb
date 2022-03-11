2312617c09f1181d3d137d8cd50065498b703312
Jeffrey <88632169+JeffreyEj@users.noreply.github.com>
2022-03-11 12:54:19 +0100

    added table

--- nodeapp/routes/home.js
+++ nodeapp/routes/home.js
@@ -4,7 +4,7 @@ let db=require('../database');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  let sql='SELECT PLAYER, UUID FROM leaderboard WHERE Gametype="Nodebuff" ORDER BY ELO DESC';
+  let sql='SELECT PLAYER, UUID, ELO FROM leaderboard WHERE Gametype="Nodebuff" ORDER BY ELO DESC LIMIT 3';
   db.query(sql, async function (err, data, fields) {
     global.row;
     global.push_player = [];
@@ -18,7 +18,7 @@ router.get('/', async function(req, res, next) {
       global.top_2 = push_player[3];
       global.top_3_name = push_player[4];  
       global.top_3 = push_player[5];
-      res.render('home', {top_1, top_1_name, top_2, top_2_name, top_3, top_3_name});
+      res.render('home', {userData: data, top_1, top_1_name, top_2, top_2_name, top_3, top_3_name});
 });
 });
 

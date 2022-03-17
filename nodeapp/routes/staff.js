216f2e0b5ac6c398413a9518aac3bd9d062f410b
Jeffrey <88632169+JeffreyEj@users.noreply.github.com>
2022-03-17 18:35:51 +0100

    implemented animated skins for staff and added a bunch more

--- nodeapp/routes/staff.js
+++ nodeapp/routes/staff.js
@@ -4,7 +4,7 @@ let db=require('../database');
 
 /* GET staff page. */
 router.get('/', async function(req, res, next) {
-    let sql='SELECT PLAYER FROM leaderboard WHERE Gametype="Owner" ORDER BY ELO DESC';
+    let sql='SELECT PLAYER FROM leaderboard WHERE Gametype="NoDebuff" ORDER BY ELO DESC';
       db.query(sql, async function (err, p_data, fields) {
         let resultArray = Object.values(JSON.parse(JSON.stringify(p_data)));
         var ee;
@@ -13,7 +13,19 @@ router.get('/', async function(req, res, next) {
           ee = p_data[key];
           pp.push(ee.PLAYER);
           });
-    res.render('staff', {pp, ee}); 
+
+          sql='SELECT PLAYER, UUID FROM leaderboard WHERE Gametype="Owner" ORDER BY ELO DESC';
+          db.query(sql, async function (err, s_data, fields) {
+            var s_ee;
+            var s_pp = [];
+            Object.keys(s_data).forEach(async function(key) {
+            s_ee = s_data[key];
+            s_pp.push(s_ee.PLAYER, s_ee.UUID);
+          });
+          
+
+    res.render('staff', {pp, ee, p_data,sUser: s_data, s_ee, s_pp}); 
+});
 });
 });
 

fb12f523990f807e2749bcf45dc7bb583864cdcc
JeffreyEj <JeffreyEjeanye@gmail.com>
2022-03-27 04:33:47 +0200

    latest version (still not working) /leaderboards

--- nodeapp/routes/leaderboards.js
+++ nodeapp/routes/leaderboards.js
@@ -4,15 +4,17 @@ let db=require('../database');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  let sql='SELECT PLAYER, UUID, ELO FROM leaderboard WHERE Gametype="Nodebuff" ORDER BY ELO DESC';
+  let sql='SELECT GAMETYPE, PLAYER, UUID, ELO FROM leaderboard WHERE Gametype="NoDebuff" ORDER BY ELO DESC';
   db.query(sql, async function (err, data, fields) {
     global.row;
     global.push_player = [];
+    global.d_gametypes = [];
     Object.keys(data).forEach(async function(key) {
       row = data[key];
       push_player.push(row.PLAYER, row.UUID);
+      d_gametypes.push(row.GAMETYPE);
       });
-
+      let d_final_game = [...new Set(d_gametypes)];
       
       sql='SELECT PLAYER FROM leaderboard WHERE Gametype="Nodebuff" ORDER BY ELO DESC';
       db.query(sql, async function (err, p_data, fields) {
@@ -35,7 +37,7 @@ router.get('/', async function(req, res, next) {
           });
           let final_gametypes = [...new Set(gametypes_gamemodes)];
           
-      res.render('leaderboards', {userData: data, p_data, push_player, resultArray, pp, ee, row, allGametypes: final_gametypes});
+      res.render('leaderboards', {userData: data, data, p_data, push_player, resultArray, pp, ee, row, allGametypes: final_gametypes, d_final_game});
 });      
 });
 });

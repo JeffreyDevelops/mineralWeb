abb7293925b6226439df2b2aa6c96adf220477d9
JeezyDev <JeffreyEjeanye@gmail.com>
2023-02-10 00:35:37 +0100

    successfully added lastSeen and firstJoined

--- nodeapp/routes/player.js
+++ nodeapp/routes/player.js
@@ -73,7 +73,79 @@ let db=require('../database');
     get_player_stats.push(row_gametype.GAMETYPE, row_gametype.PLAYER, row_gametype.ELO);
   });
 
-    res.render('player', {userData: data, PlayerPerPage, row, push_player, parameter, resultArray, rank_name, player_show_skin, row_nod, nod_array, nod_elo_array, gametypes, row_gametype, gametypes_player, allGametypes: final_gametypes, gametypes_elo, get_player_stats});  
+  sql = `SELECT * FROM status WHERE playerName = '${parameter}'`;
+  db.query(sql, function (err, status_data, fields) {
+    global.row_status;
+    global.row_status_first_joined;
+    global.row_status_last_seen;
+    global.unixTimeZeroJoined;
+    global.unixTimeZeroLastSeen;
+    global.final_firstJoined;
+    global.final_lastSeen;
+    global.online_status = 0;
+    global.date1 = new Date();
+    global.date2;
+  Object.keys(status_data).forEach(function(key) {
+    row_status = status_data[key];
+    online_status = row_status.online;
+
+    row_status_first_joined = row_status.firstJoined;
+    
+    unixTimeZero = Date.parse(row_status_first_joined);
+    let dateFormat = new Date(unixTimeZero)
+
+    final_firstJoined = dateFormat.getDate()+
+    "/"+(dateFormat.getMonth()+1)+
+    "/"+dateFormat.getFullYear();
+
+    row_status_last_seen = row_status.lastSeen;
+    unixTimeZeroLastSeen = Date.parse(row_status_last_seen);
+    final_lastSeen = new Date(unixTimeZeroLastSeen)
+
+  });
+
+  function timeDifference(date1, date2) {
+    var difference = date1.getTime() - date2.getTime();
+
+    var daysDifference = Math.floor(difference/1000/60/60/24);
+    difference -= daysDifference*1000*60*60*24
+
+    var hoursDifference = Math.floor(difference/1000/60/60);
+    difference -= hoursDifference*1000*60*60
+
+    var minutesDifference = Math.floor(difference/1000/60);
+    difference -= minutesDifference*1000*60
+
+    if (isNaN(daysDifference) || isNaN(hoursDifference) || isNaN(minutesDifference) ||
+      daysDifference === 0 && hoursDifference === 0 && minutesDifference === 0) {
+      final_lastSeen = 'OFFLINE'; 
+      return;
+    }
+    
+
+    if (daysDifference === 0) {
+      final_lastSeen = hoursDifference + ' hour/s ago'; 
+    } else if (hoursDifference === 0) {
+      final_lastSeen = minutesDifference + ' minute/s ago ';
+    } else {
+      final_lastSeen = daysDifference + ' day/s ago'; 
+    }
+    
+
+    /*
+    final_lastSeen =
+      daysDifference + ' day/s ' + 
+      hoursDifference + ' hour/s ' + 
+      minutesDifference + ' minute/s '
+      */
+}
+try {
+  timeDifference(date1, final_lastSeen);
+} catch (error) {
+}
+ 
+    res.render('player', {userData: data, PlayerPerPage, row, push_player, parameter, resultArray, rank_name, player_show_skin, row_nod, nod_array, nod_elo_array, gametypes, row_gametype, gametypes_player, allGametypes: final_gametypes, gametypes_elo, get_player_stats, online_status});  
+});
 }); 
 });   
 });

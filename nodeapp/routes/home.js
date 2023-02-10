542a3b73a7baf793361beff553f6c17622485dd9
JeezyDev <JeffreyEjeanye@gmail.com>
2023-02-10 02:55:20 +0100

    recoded home.js changed sql and simplified code

--- nodeapp/routes/home.js
+++ nodeapp/routes/home.js
@@ -4,13 +4,16 @@ let db=require('../database');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  let sql='SELECT PLAYER, UUID, ELO FROM leaderboard WHERE Gametype="Nodebuff" ORDER BY ELO DESC';
-  db.query(sql, async function (err, data, fields) {
+  db.query('SELECT `PLAYER`, `UUID`, `ELO` FROM `leaderboard` WHERE `Gametype`= ? ORDER BY `ELO` DESC',
+  ['NoDebuff'],
+    async function (err, data, fields) {
     global.row;
     global.push_player = [];
+    global.pp = [];
     Object.keys(data).forEach(async function(key) {
       row = data[key];
       push_player.push(row.PLAYER, row.UUID);
+      pp.push(row.PLAYER);
       });
       global.top_1_name = push_player[0];
       global.top_1 = push_player[1];
@@ -19,22 +22,8 @@ router.get('/', async function(req, res, next) {
       global.top_3_name = push_player[4];  
       global.top_3 = push_player[5];
 
-      
-      sql='SELECT PLAYER FROM leaderboard WHERE Gametype="Nodebuff" ORDER BY ELO DESC';
-      db.query(sql, async function (err, p_data, fields) {
-        let resultArray = Object.values(JSON.parse(JSON.stringify(p_data)));
-        var ee;
-        var pp = [];
-        Object.keys(p_data).forEach(async function(key) {
-          ee = p_data[key];
-          pp.push(ee.PLAYER);
-          });
-          
-      res.render('home', {userData: data, p_data, push_player, resultArray, pp, ee, row, top_1, top_1_name, top_2, top_2_name, top_3, top_3_name});
+      res.render('home', {userData: data, push_player, pp, row, top_1, top_1_name, top_2, top_2_name, top_3, top_3_name});
 });
 });
-});
-
-
 
 module.exports = router;

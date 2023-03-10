6969cdf20698d3304c84ed67ec81447103ce12a4
JeezyDev <JeffreyEjeanye@gmail.com>
2023-03-10 01:07:58 +0100

    fixed leaderboards.js fixed search logic as well now can search through all players now

--- nodeapp/routes/leaderboards.js
+++ nodeapp/routes/leaderboards.js
@@ -10,18 +10,24 @@ router.get('/', async function(req, res, next) {
     global.push_player_elo = [];
     global.push_player_player = [];
     global.push_player_UUID = [];
-    global.push_player = [];
     global.d_gametypes = [];
     global.leaderboards_data = [];
     Object.keys(data).forEach(async function(key) {
       row = data[key];
-      push_player.push(row.PLAYER, row.UUID);
       push_player_elo.push(row.ELO);
       push_player_player.push(row.PLAYER);
       push_player_UUID.push(row.UUID);
       d_gametypes.push(row.GAMETYPE);
       leaderboards_data.push(row.GAMETYPE, row.PLAYER, row.UUID, row.ELO);
       });
+
+      db.query('SELECT `playerName`, `playerUUID` FROM `status`;',
+      async function (err, data, fields) {
+        global.push_player = [];
+        Object.keys(data).forEach(function(key) {
+        row = data[key];
+        push_player.push(row.playerName, row.playerUUID);
+    });
 
       db.query('SELECT `PLAYER` FROM `elo` WHERE `Gametype` = ? ORDER BY `ELO` DESC;',
       ['NoDebuff'],
@@ -48,6 +54,7 @@ router.get('/', async function(req, res, next) {
           
       res.render('leaderboards', {userData: data, data, p_data, push_player, resultArray, pp, ee, row, allGametypes: final_gametypes, push_player_elo,  push_player_player, push_player_UUID, d_gametypes, leaderboards_data});
 });      
+});
 });
 });
 });

c446bf194305804c0d60041edf903c3d73d2df89
JeffreyDevelops <JeffreyEjeanye@gmail.com>
2025-02-15 04:40:50 +0100

    recoded leaderboards

--- nodeapp/routes/leaderboards.js
+++ nodeapp/routes/leaderboards.js
@@ -4,48 +4,57 @@ let db=require('../database');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  db.conn_practice.query('SELECT `GAMETYPE`, `PLAYER`, `UUID`, `ELO` FROM `elo` ORDER BY `GAMETYPE` DESC, `ELO` DESC;',
+
+  db.conn_core.query('SELECT `playerName`, `playerUUID` FROM `players`;',
     async function (err, data, fields) {
-    global.row;
-    global.push_player_elo = [];
-    global.push_player_player = [];
-    global.push_player_UUID = [];
-    global.d_gametypes = [];
-    global.leaderboards_data = [];
-    Object.keys(data).forEach(async function(key) {
+      global.push_player = [];
+      Object.keys(data).forEach(function(key) {
       row = data[key];
-      push_player_elo.push(row.ELO);
-      push_player_player.push(row.PLAYER);
-      push_player_UUID.push(row.UUID);
-      d_gametypes.push(row.GAMETYPE);
-      leaderboards_data.push(row.GAMETYPE, row.PLAYER, row.UUID, row.ELO);
+      push_player.push(row.playerName, row.playerUUID);
+  });
+
+  db.conn_practice.query('SELECT * FROM `elo` ORDER BY `GAMETYPE` DESC;', 
+    async function (err, gametype_data, fields) {
+      global.gametypes_gamemodes = [];
+
+      Object.keys(gametype_data).forEach(function(key) {
+        row_gametype = gametype_data[key];
+        gametypes_gamemodes.push({gametype: row_gametype.GAMETYPE, players: []});
       });
 
-      db.conn_core.query('SELECT `playerName`, `playerUUID` FROM `players`;',
-      async function (err, data, fields) {
-        global.push_player = [];
-        Object.keys(data).forEach(function(key) {
-        row = data[key];
-        push_player.push(row.playerName, row.playerUUID);
-    });
+      let leaderboardsData = gametypes_gamemodes.filter((game, index, self) => 
+        index === self.findIndex(g => g.gametype === game.gametype)
+      );
 
+      let setGlobalData = leaderboardsData.unshift({gametype: 'Global', players: []});
+      
+    
+          await Promise.all(leaderboardsData.map(async (obj) => {
+                return new Promise((resolve, reject) => {
+                  db.conn_core.query(
+                    "SELECT * FROM `elo` WHERE `GAMETYPE` = ? ORDER BY `elo` DESC ",
+                    [obj.gametype],
+                    (err, so_data) => {
+                      if (err) return reject(err);
+                      
+                      obj.players = so_data.map((player) => ({
+                        playerName: player.PLAYER,
+                        playerUUID: player.UUID,
+                        elo: player.ELO
+                      }));
+                      resolve(obj);
+                    }
+                  );
+                });
+              })
+            );
 
-        db.conn_practice.query('SELECT `GAMETYPE`, `PLAYER`, `ELO` FROM `elo` ORDER BY `PLAYER` DESC;', 
-        async function (err, gametype_data, fields) {
-          global.gametypes_gamemodes = [];
+      console.log(leaderboardsData);
 
-          Object.keys(gametype_data).forEach(function(key) {
-            row_gametype = gametype_data[key];
-            gametypes_gamemodes.push(row_gametype.GAMETYPE);
-          });
-          let final_gametypes = [...new Set(gametypes_gamemodes)];
-          
-      res.render('leaderboards', {userData: data, data, push_player, row, allGametypes: final_gametypes, push_player_elo,  push_player_player, push_player_UUID, d_gametypes, leaderboards_data});
-});      
+      res.render('leaderboards', { push_player, leaderboardsData});
+   
 });
 });
 });
-
-
 
 module.exports = router;

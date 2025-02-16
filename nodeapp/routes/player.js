a6eb74b012abdbb7c6bc14be173e6dac217fe219
JeffreyDevelops <JeffreyEjeanye@gmail.com>
2025-02-16 22:27:56 +0100

    fixed player stats (added global elo)

--- nodeapp/routes/player.js
+++ nodeapp/routes/player.js
@@ -1,185 +1,174 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-let db=require('../database');
+let db = require("../database");
 
 /* GET player page. */
-  router.get('/:username', function(req, res, next) {
-  db.conn_practice.query('SELECT `PLAYER`, `UUID` FROM `elo` WHERE `Gametype` = ? ORDER BY `ELO` DESC;',
-  ["NoDebuff"],
-  async function (err, data, fields) {
-  let parameter = req.params.username;
-  global.row;
-  let resultArray = Object.values(JSON.parse(JSON.stringify(data)));
-  let PlayerPerPage = 1;  
-  Object.keys(data).forEach(function(key) {
-    row = data[key];
-  });
-
-  db.conn_core.query('SELECT `playerName`, `playerUUID` FROM `players`;',
-  async function (err, data, fields) {
-    global.push_player = [];
-    Object.keys(data).forEach(function(key) {
-      row = data[key];
-      push_player.push(row.playerName, row.playerUUID);
-    });
-
-    db.conn_core.query('SELECT * FROM `ranks`;',
+router.get("/:username", function (req, res, next) {
+  db.conn_practice.query(
+    "SELECT `PLAYER`, `UUID` FROM `elo` WHERE `Gametype` = ? ORDER BY `ELO` DESC;",
+    ["NoDebuff"],
     async function (err, data, fields) {
-      global.ranks = [];
-      Object.keys(data).forEach(function(key) {
-        getRanks = data[key];
-        ranks.push({rankName: getRanks.rankName, rgb: getRanks.rankRGB});
+      let parameter = req.params.username;
+      global.row;
+      Object.keys(data).forEach(function (key) {
+        row = data[key];
       });
-  
-  
-  db.conn_core.query('SELECT * FROM `players` WHERE `playerName` LIKE ?;',
-  [`%${parameter}%`],
-  async function (err, rank_data, fields) {
-  global.row_rank;
-  global.rank_name = "";
-  Object.keys(rank_data).forEach(function(key) {
-    row_rank = rank_data[key];
-    rank_name = row_rank.rank;
-  });
 
-  db.conn_core.query('SELECT * FROM `players` WHERE `playerName` = ?;',
-  [parameter],
-  async function (err, skin_data, fields) {
-  global.row_skin;
-  global.player_show_skin;
-  Object.keys(skin_data).forEach(function(key) {
-    row_skin = skin_data[key];
-    player_show_skin = row_skin.playerUUID;
-  });
+      db.conn_core.query(
+        "SELECT `playerName`, `playerUUID` FROM `players`;",
+        async function (err, data, fields) {
+          global.push_player = [];
+          Object.keys(data).forEach(function (key) {
+            row = data[key];
+            push_player.push(row.playerName, row.playerUUID);
+          });
 
-  db.conn_practice.query('SELECT `PLAYER`, `ELO`, `GAMETYPE` FROM `elo` WHERE `Gametype` = ? ORDER BY `ELO` DESC;',
-  ["NoDebuff"],
-  async function (err, nod_data, fields) {
-  global.row_nod;
-  global.nod_array = [];
-  global.nod_elo_array = [];
-  Object.keys(nod_data).forEach(function(key) {
-    row_nod = nod_data[key];
-    nod_array.push(row_nod.PLAYER);
-    nod_elo_array.push(row_nod.ELO);
-  });
+          db.conn_core.query(
+            "SELECT * FROM `ranks`;",
+            async function (err, data, fields) {
+              global.ranks = [];
+              Object.keys(data).forEach(function (key) {
+                getRanks = data[key];
+                ranks.push({
+                  rankName: getRanks.rankName,
+                  rgb: getRanks.rankRGB,
+                });
+              });
 
-  db.conn_practice.query('SELECT `GAMETYPE`, `PLAYER`, `ELO` FROM `elo`  WHERE `PLAYER` = ? ORDER BY `PLAYER` DESC;',
-  [parameter],
-  async function (err, gametype_data, fields) {
-  global.gametypes = [];
-  global.gametypes_player = [];
-  global.gametypes_elo = [];
-  global.gametypes_gamemodes = [];
-  Object.keys(gametype_data).forEach(function(key) {
-    row_gametype = gametype_data[key];
-    gametypes_gamemodes.push(row_gametype.GAMETYPE);
-    gametypes.push(row_gametype.ELO);
-    gametypes_player.push(row_gametype.PLAYER);
-    gametypes_elo.push(row_gametype.GAMETYPE, row_gametype.PLAYER, row_gametype.ELO);
-  });
+              db.conn_core.query(
+                "SELECT * FROM `players` WHERE `playerName` = ?;",
+                [parameter],
+                async function (err, data, fields) {
+                  let playerAndRankData;
+                  Object.keys(data).forEach(function (key) {
+                    player_data = data[key];
+                    playerAndRankData = ranks.map((obj) => ({
+                      ...obj,
+                      playerUUID: player_data.playerUUID,
+                      playerRankName: player_data.rank,
+                    }));
+                  });
 
-  let final_gametypes = [...new Set(gametypes_gamemodes)];
+                  db.conn_practice.query(
+                    "SELECT * FROM `elo` WHERE `PLAYER` = ? ORDER BY `elo` DESC;",
+                    [parameter],
+                    async function (err, gametype_data, fields) {
+                      global.leaderboardsData = [];
 
-  db.conn_practice.query('SELECT * FROM `elo` WHERE `PLAYER` = ?;',
-  [parameter],
-  async function (err, player_data, fields) {
-  global.get_player_stats = [];
-  Object.keys(player_data).forEach(function(key) {
-    row_gametype = player_data[key];
-    get_player_stats.push(row_gametype.GAMETYPE, row_gametype.PLAYER, row_gametype.ELO);
-  });
+                      Object.keys(gametype_data).forEach(function (key) {
+                        row_gametype = gametype_data[key];
+                        leaderboardsData.push({
+                          gametype: row_gametype.GAMETYPE,
+                          elo: row_gametype.ELO,
+                        });
+                      });
 
-  db.conn_core.query('SELECT * FROM `players` WHERE `playerName` = ?',
-  [parameter],
-  async function (err, status_data, fields) {
-    global.row_status;
-    global.row_status_first_joined;
-    global.row_status_last_seen;
-    global.unixTimeZeroJoined;
-    global.unixTimeZeroLastSeen;
-    global.final_firstJoined;
-    global.final_lastSeen;
-    global.online_status = 0;
-    global.date1 = new Date();
-    global.date2;
-  Object.keys(status_data).forEach(function(key) {
-    row_status = status_data[key];
-    online_status = row_status.online;
+                    if (leaderboardsData.length !== 0) {
+                      const totalGamemodes = 20;
+                      let totalElo = leaderboardsData.reduce((acc, obj) => acc + obj.elo, 1);
+                      let globalElo = Math.round((totalElo + 1000 * (totalGamemodes - leaderboardsData.length)) / 20);
+                      leaderboardsData.unshift({ gametype: "Global", elo: globalElo });
+                    }
+                
+                      db.conn_core.query(
+                        "SELECT * FROM `players` WHERE `playerName` = ?",
+                        [parameter],
+                        async function (err, status_data, fields) {
+                          global.row_status;
+                          global.row_status_first_joined;
+                          global.row_status_last_seen;
+                          global.unixTimeZeroJoined;
+                          global.unixTimeZeroLastSeen;
+                          global.final_firstJoined;
+                          global.final_lastSeen;
+                          global.online_status = 0;
+                          global.date1 = new Date();
+                          global.date2;
+                          Object.keys(status_data).forEach(function (key) {
+                            row_status = status_data[key];
+                            online_status = row_status.online;
 
-    row_status_first_joined = row_status.firstJoined;
-    
-    unixTimeZero = Date.parse(row_status_first_joined);
-    let dateFormat = new Date(unixTimeZero)
+                            row_status_first_joined = row_status.firstJoined;
 
-    final_firstJoined = dateFormat.getDate()+
-    "/"+(dateFormat.getMonth()+1)+
-    "/"+dateFormat.getFullYear();
+                            unixTimeZero = Date.parse(row_status_first_joined);
+                            let dateFormat = new Date(unixTimeZero);
 
-    row_status_last_seen = row_status.lastSeen;
-    unixTimeZeroLastSeen = Date.parse(row_status_last_seen);
-    final_lastSeen = new Date(unixTimeZeroLastSeen)
+                            final_firstJoined =
+                              dateFormat.getDate() +
+                              "/" +
+                              (dateFormat.getMonth() + 1) +
+                              "/" +
+                              dateFormat.getFullYear();
 
-  });
+                            row_status_last_seen = row_status.lastSeen;
+                            unixTimeZeroLastSeen =
+                              Date.parse(row_status_last_seen);
+                            final_lastSeen = new Date(unixTimeZeroLastSeen);
+                          });
 
-  function timeDifference(date1, date2) {
-    var difference = date1.getTime() - date2.getTime();
+                          function timeDifference(date1, date2) {
+                            var difference = date1.getTime() - date2.getTime();
 
-    var daysDifference = Math.floor(difference/1000/60/60/24);
-    difference -= daysDifference*1000*60*60*24
+                            var daysDifference = Math.floor(
+                              difference / 1000 / 60 / 60 / 24
+                            );
+                            difference -= daysDifference * 1000 * 60 * 60 * 24;
 
-    var hoursDifference = Math.floor(difference/1000/60/60);
-    difference -= hoursDifference*1000*60*60
+                            var hoursDifference = Math.floor(
+                              difference / 1000 / 60 / 60
+                            );
+                            difference -= hoursDifference * 1000 * 60 * 60;
 
-    var minutesDifference = Math.floor(difference/1000/60);
-    difference -= minutesDifference*1000*60
+                            var minutesDifference = Math.floor(
+                              difference / 1000 / 60
+                            );
+                            difference -= minutesDifference * 1000 * 60;
 
-    if (isNaN(daysDifference) || isNaN(hoursDifference) || isNaN(minutesDifference) ||
-      daysDifference === 0 && hoursDifference === 0 && minutesDifference === 0) {
-      final_lastSeen = 'OFFLINE'; 
-      return;
+                            if (
+                              isNaN(daysDifference) ||
+                              isNaN(hoursDifference) ||
+                              isNaN(minutesDifference) ||
+                              (daysDifference === 0 &&
+                                hoursDifference === 0 &&
+                                minutesDifference === 0)
+                            ) {
+                              final_lastSeen = "OFFLINE";
+                              return;
+                            }
+
+                            if (daysDifference > 0) {
+                              final_lastSeen = daysDifference + " day/s ago";
+                            }
+                            if (hoursDifference > 0 && daysDifference <= 0) {
+                              final_lastSeen = hoursDifference + " hour/s ago";
+                            }
+
+                            if (minutesDifference > 0 && hoursDifference <= 0) {
+                              final_lastSeen =
+                                minutesDifference + " minute/s ago ";
+                            }
+                          }
+                          try {
+                            timeDifference(date1, final_lastSeen);
+                          } catch (error) {}
+
+                          res.render("player", {
+                            parameter,
+                            push_player,
+                            playerAndRankData,
+                            online_status,
+                          });
+                        }
+                      );
+                    }
+                  );
+                }
+              );
+            }
+          );
+        }
+      );
     }
-
-
-    console.log(daysDifference + ' day/s ' + 
-    hoursDifference + ' hour/s ' + 
-    minutesDifference + ' minute/s ');
-
-
-    if (daysDifference > 0) {
-      final_lastSeen = daysDifference + ' day/s ago'; 
-      
-    } 
-     if (hoursDifference > 0 && daysDifference <= 0) {
-      final_lastSeen = hoursDifference + ' hour/s ago'; 
-    } 
-      
-    if (minutesDifference > 0 && hoursDifference <= 0) {
-      final_lastSeen = minutesDifference + ' minute/s ago ';
-    }
-    
-
-    /*
-    final_lastSeen =
-      daysDifference + ' day/s ' + 
-      hoursDifference + ' hour/s ' + 
-      minutesDifference + ' minute/s '
-      */
-}
-try {
-  timeDifference(date1, final_lastSeen);
-} catch (error) {
-}
- 
-    res.render('player', {userData: data, PlayerPerPage, row, push_player, ranks, parameter, resultArray, rank_name, player_show_skin, row_nod, nod_array, nod_elo_array, gametypes, row_gametype, gametypes_player, allGametypes: final_gametypes, gametypes_elo, get_player_stats, online_status});  
-});
-});   
-});
-});
-}); 
-});
-});
-});
-});
+  );
 });
 module.exports = router;

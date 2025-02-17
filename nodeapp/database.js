612fd09edd349d4b1e4168f0aad88f8892a70b4e
JeffreyDevelops <JeffreyEjeanye@gmail.com>
2025-02-17 17:11:09 +0100

    added port to database.js

--- nodeapp/database.js
+++ nodeapp/database.js
@@ -2,6 +2,7 @@ let mysql = require('mysql2');
 let get_secret = require('./secrets_DB.json');
 let mineral = mysql.createPool({
   host: get_secret.db.host, 
+  port: get_secret.db.port,
   user: get_secret.db.user,   
   password: get_secret.db.password,     
   database: get_secret.db.database
